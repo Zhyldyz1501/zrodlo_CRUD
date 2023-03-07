2d9722b3fc18c33b53f38c2cab4fa38cc7702caa
Zhyldyz <jargynbaeva@gmail.com>
2023-03-07 17:14:45 +0100

    'last changes'

--- server/index.js
+++ server/index.js
@@ -1,17 +1,15 @@
 import express from 'express';
 import * as dotenv from 'dotenv';
 import cors from 'cors';
-import intialDbConnection from './mongodb/connect.js';
 import userRouter from './routes/user.routes.js';
 import propertyRouter from './routes/property.routes.js';
+import connectDB from './mongodb/connect.js';
 
 dotenv.config();
 
 const app = express();
 app.use(cors());
 app.use(express.json({ limit: '50mb' }));
-const { intialDbConnection } = require("./mongodb/connect.js");
-intialDbConnection();
 app.get('/', (req, res) => {
     res.send({message: 'Hello World'})
 })
@@ -21,7 +19,7 @@ app.use("/properties", propertyRouter)
 
 const startServer = async () => {
     try {
-        intialDbConnection(process.env.MONGODB_URL)
+        connectDB(process.env.MONGODB_URL)
         app.listen(8080, () =>
           console.log(
             "Server started listening on port http://localhost:8080"
--- server/mongodb/connect.js
+++ server/mongodb/connect.js
@@ -1,25 +1,12 @@
-const intialDbConnection = async () => {
-  try {
-    await mongoose.connect(mySecret, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
-    console.log("db connected");
-  } catch (error) {
-    console.error(error);
-  }
-};
 
-module.exports = { intialDbConnection };
+import mongoose from 'mongoose';
 
-// import mongoose from 'mongoose';
+const connectDB = (url) => {
+    mongoose.set('strictQuery', true);
 
-// const connectDB = (url) => {
-//     mongoose.set('strictQuery', true);
-
-//     mongoose
-//         .connect(url)
-//         .then(() => console.log("MongoDB connected"))
-//         .catch((error) => console.log(error));
-// }
-// export default connectDB
+    mongoose
+        .connect(url)
+        .then(() => console.log("MongoDB connected"))
+        .catch((error) => console.log(error));
+}
+export default connectDB
